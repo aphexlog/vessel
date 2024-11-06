8d01e6652b22e1c1d29f9c9b1c118a73a612b5b9
Aaron West <aphexlog@gmail.com>
2024-11-05 18:39:37 -0600

    adding randomness 1..10

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+//use rand::random;
+use rand::Rng;
 use std::io;
 
 fn main() {
@@ -12,4 +14,26 @@ fn main() {
         .expect("Failed to read line");
 
     println!("You guessed: {}", guess);
+
+    //number between 1-10
+    let secret_number: u32 = rand::thread_rng().gen_range(1..=10);
+
+    println!("The secret number is: {}", secret_number);
+
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+    while guess != secret_number {
+        println!("Please input your guess.");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        println!("You guessed: {}", guess);
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        match guess.cmp(&secret_number) {
+            std::cmp::Ordering::Less => println!("Too small!"),
+            std::cmp::Ordering::Greater => println!("Too big!"),
+            std::cmp::Ordering::Equal => println!("You win!"),
+        }
+    }
 }
